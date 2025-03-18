b578bd32d3526056eee84c81a9214df94bc6e0d9
PatuLang <patrik.laang@gmail.com>
2025-03-18 18:38:02 +0200

    entry point file update

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -7,12 +7,14 @@ echo "Applying MariaDB configuration..."
 cat << EOF > /etc/my.cnf.d/mariadb-server.cnf
 [mysqld]
 user=mysql
+socket=/run/mysqld/mysqld.sock
+pid-file=/run/mysqld/mariadb.pid
+log_error=/var/log/mysql/error.log
+port=3306
 bind-address=0.0.0.0
 skip-networking=0
 skip-name-resolve
 skip-host-cache
-socket=/run/mysqld/mysqld.sock
-port=3306
 
 EOF
 # fi
@@ -24,16 +26,21 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Starting MariaDB temporarily..."
 	mysqld --user=mysql --skip-networking --socket=/var/run/mysqld/mysqld.sock &
 
+    echo "Waiting for MariaDB to start..."
     until mysqladmin ping --socket=/var/run/mysqld/mysqld.sock --silent; do
 		    sleep 2
 	done
 
     echo "Setting up users and database..."
+    mysql -u root -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
     mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
     mysql -u root -e "FLUSH PRIVILEGES;"
+    
+    echo "Shutting down temporary MariaDB..."
+    mysqladmin shutdown --socket=/var/run/mysqld/mysqld.sock -uroot -p"$MYSQL_ROOT_PASSWORD"
 fi
 
-mysqladmin shutdown
 exec mysqld_safe
