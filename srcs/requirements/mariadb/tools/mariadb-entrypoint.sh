55b5335e6567dc605f133e80742edef676e8a914
PatuLang <patrik.laang@gmail.com>
2025-03-19 16:40:38 +0200

    entry point file update

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -6,30 +6,20 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db --rpm > /dev/null
 
-    # echo "Starting MariaDB temporarily..."
-	# mysqld --user=mysql --skip-networking --socket=/var/run/mysqld/mysqld.sock &
-
-    # echo "Waiting for MariaDB to start..."
-    # until mysqladmin ping --socket=/var/run/mysqld/mysqld.sock --silent; do
-	# 	    sleep 2
-	# done
-
     echo "Setting up database and users..."
 	mysqld --bootstrap --datadir=/var/lib/mysql --user=mysql <<-EOF
 
-        USE mysql;
-		FLUSH PRIVILEGES;
-		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
-		FLUSH PRIVILEGES;
+    USE mysql;
+	FLUSH PRIVILEGES;
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+	FLUSH PRIVILEGES;
 	EOF
 
-    # echo "Shutting down temporary MariaDB..."
-    # mysqladmin shutdown --socket=/var/run/mysqld/mysqld.sock -uroot -p"$MYSQL_ROOT_PASSWORD"
 fi
 
 echo "Starting MariaDB"
