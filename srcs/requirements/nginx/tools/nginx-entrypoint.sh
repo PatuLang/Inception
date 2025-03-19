8e3acd26d0af7caacc0b8c0655322011fa4c6475
PatuLang <patrik.laang@gmail.com>
2025-03-19 19:39:30 +0200

    entry point file update

--- srcs/requirements/nginx/tools/nginx-entrypoint.sh
+++ srcs/requirements/nginx/tools/nginx-entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Waiting for WordPress to be fully up..."
-until nc -z $WORDPRESS_HOST $WORDPRESS_PORT; do
-    echo "Waiting for WordPress ($WORDPRESS_HOST:$WORDPRESS_PORT)..."
+until curl -sSf "http://$WORDPRESS_HOST/wp-login.php" > /dev/null; do
     sleep 2
 done
+
 echo "✅ WordPress is up!"
 
 cat << EOF >> /etc/nginx/http.d/default.conf
@@ -18,7 +18,6 @@ server {
     ssl_certificate /etc/nginx/ssl/plang.crt;
     ssl_certificate_key /etc/nginx/ssl/plang.key;
     ssl_protocols TLSv1.3;
-    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
 
     root /var/www/html;
     index index.php index.html index.htm;
@@ -30,12 +29,12 @@ server {
     location ~ [^/]\.php(/|\$) {
         try_files \$fastcgi_script_name =404;
 
+        include fastcgi_params;
         fastcgi_pass wordpress:9000;
         fastcgi_index index.php;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         fastcgi_param PATH_INFO \$fastcgi_path_info;
         fastcgi_split_path_info ^(.+\.php)(/.*)\$;
-        include fastcgi_params;
     }
 }
 EOF
